7d662a74410aac7d5b696281306c483ad2f4c4f8
hungpv-3077 <pham.van.hung-b@sun-asterisk.com>
2026-01-21 10:19:57 +0700

    init

---
+++ src/test/java/sun/asterisk/booking_tour/BookingTourApplicationTests.java
@@ -0,0 +1,13 @@
+package sun.asterisk.booking_tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
